f3f3eb9b72470c182b2d5de3da9b3e24c471112f
SJH <s.j.hawes@btinternet.com>
2022-09-17 17:01:00 +0100

    Initial

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/phd_law/admin/phd_todo_2.org" "/home/telemachus/life_org/todo/sep_22.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
