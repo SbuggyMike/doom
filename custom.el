1bf18d772b0e16b6b2077e1512e401b6de4eea2c
sbuggyMike <s.j.hawes@btinternet.com>
2022-12-10 15:36:26 +0000

    added org agenda files

--- custom.el
+++ custom.el
@@ -3,10 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/todo.org" "/home/hector/phd_law/admin/phd_todo_2.org")))
+ '(org-agenda-files
+   '("~/life_org/habits_gtd.org" "/home/hector/life_org/gtd.org" "/home/hector/life_org/todo.org" "/home/hector/phd_law/admin/phd_todo_2.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(with-eval-after-load 'ox-latex
+  (add-to-list 'org-latex-classes '("letter" "\\documentclass{letter}"))
+  )
